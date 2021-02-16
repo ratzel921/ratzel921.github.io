206a9e69751dec2f3ffb7c4a04fea54ac992444c
dr079 <dr079@hdm-stuttgart.de>
2021-02-16 11:57:27 +0100

    Added HinH Project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
+// import 'roboto-fontface/css/roboto/roboto-fontface.css'
 
 import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
 Vue.component('ValidationProvider', ValidationProvider);
