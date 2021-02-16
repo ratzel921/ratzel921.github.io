eff9ad5a80ed8b0c7f243eaccf5755dd22df97ae
dr079 <dr079@hdm-stuttgart.de>
2021-02-16 22:16:28 +0100

    fixed error

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
-// Removes 'console.log' on production builds
-//'vue-cli-service build' runs production build
-// Note: Running 'vue-cli-service serve' will not remove console.log which is intended
-const removeConsolePlugin = []
-if (process.env.NODE_ENV === 'production') {
-  removeConsolePlugin.push("transform-remove-console")
-}
 module.exports = {
   presets: [
-    '@vue/app'
-  ],
-  plugins: removeConsolePlugin
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
