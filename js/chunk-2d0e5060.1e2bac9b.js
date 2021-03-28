09779216af71e12356e88182277c5716d698774d
ratzel921 <34986176+ratzel921@users.noreply.github.com>
2021-03-28 11:17:43 +0000

    Update master to output generated at cc356ee

--- js/chunk-2d0e5060.1e2bac9b.js
+++ js/chunk-2d0e5060.1e2bac9b.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5060"],{"935e":function(n,e,t){"use strict";t.r(e);var r=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-container",[t("v-row",{attrs:{align:"center",justify:"center"}},[t("h1",[n._v("Webshop")])]),t("v-row",{attrs:{align:"center",justify:"center"}},[t("h3",[n._v("This is still in progress")])])],1)},a=[],s={name:"WebShop",data:function(){return{}}},i=s,o=t("2877"),c=t("6544"),u=t.n(c),l=t("a523"),p=t("0fd9"),w=Object(o["a"])(i,r,a,!1,null,null,null);e["default"]=w.exports;u()(w,{VContainer:l["a"],VRow:p["a"]})}}]);
-//# sourceMappingURL=chunk-2d0e5060.fa822f1d.js.map
+//# sourceMappingURL=chunk-2d0e5060.1e2bac9b.js.map
