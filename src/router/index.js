3172dd44b55bbaca295b393a15e1301714e22240
dr079 <dr079@hdm-stuttgart.de>
2024-09-03 11:41:17 +0200

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,78 +1,75 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-    path: '/MiniSpielehalle',
-    name: 'MiniSpielehalle',
-    component: () => import('../projects/MiniSpielehalle.vue')
-  },
-  {
-    path: '/ParkingControlSystem',
-    name: 'Parking Control System',
-    component: () => import('../projects/ParkingControlSystem.vue')
-  },
-  {
-    path: '/WebShop',
-    name: 'WebShop',
-    component: () => import('../projects/WebShop.vue')
-  },
-  {
-    path: '/DeviceManagement',
-    name: 'DeviceManagement',
-    component: () => import('../projects/DeviceManagement.vue')
-  },
-  {
-    path: '/HandInHand',
-    name: 'HandInHand',
-    component: () => import('../projects/HandInHand.vue')
-  },
-  {
-    path: '/BeGuided',
-    name: 'BeGuided',
-    component: () => import('../projects/BeGuided.vue')
-  },
-  {
-    path: '/imprint',
-    name: 'Imprint',
-    component: () => import('../views/FooterSites/Imprint.vue')
-  },
-  {
-    path: '/privacy',
-    name: 'Privacy',
-    component: () => import('../views/FooterSites/Privacy.vue')
-  },
-  // {
-  //   path: '/projects',
-  //   name: 'Projects',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
-  // },
-  // {
-  //   path: '/skills',
-  //   name: 'Skills',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/Skills.vue')
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
-  // {
-  //   path: '/contact',
-  //   name: 'Contact',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
-  // },
-]
-
-const router = new VueRouter({
-  routes
-})
-
-export default router
+export const router = new Router({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: () => import('../views/Home.vue')
+        },
+        {
+            path: '/MiniSpielehalle',
+            name: 'MiniSpielehalle',
+            component: () => import('../projects/MiniSpielehalle.vue')
+        },
+        {
+            path: '/ParkingControlSystem',
+            name: 'Parking Control System',
+            component: () => import('../projects/ParkingControlSystem.vue')
+        },
+        {
+            path: '/WebShop',
+            name: 'WebShop',
+            component: () => import('../projects/WebShop.vue')
+        },
+        {
+            path: '/DeviceManagement',
+            name: 'DeviceManagement',
+            component: () => import('../projects/DeviceManagement.vue')
+        },
+        {
+            path: '/HandInHand',
+            name: 'HandInHand',
+            component: () => import('../projects/HandInHand.vue')
+        },
+        {
+            path: '/BeGuided',
+            name: 'BeGuided',
+            component: () => import('../projects/BeGuided.vue')
+        },
+        {
+            path: '/imprint',
+            name: 'Imprint',
+            component: () => import('../views/FooterSites/Imprint.vue')
+        },
+        {
+            path: '/privacy',
+            name: 'Privacy',
+            component: () => import('../views/FooterSites/Privacy.vue')
+        },
+        // {
+        //   path: '/projects',
+        //   name: 'Projects',
+        //   component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
+        // },
+        // {
+        //   path: '/skills',
+        //   name: 'Skills',
+        //   component: () => import(/* webpackChunkName: "about" */ '../views/Skills.vue')
+        // },
+        // {
+        //   path: '/about',
+        //   name: 'About',
+        //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        // },
+        // {
+        //   path: '/contact',
+        //   name: 'Contact',
+        //   component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+        // },
+    ]
+});
