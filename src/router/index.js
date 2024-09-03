c12ad3eaf44c32ecc356ca7123cf898cab74ebce
dr079 <dr079@hdm-stuttgart.de>
2024-09-03 11:52:19 +0200

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,75 +1,64 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export const router = new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: () => import('../views/Home.vue')
-        },
-        {
-            path: '/MiniSpielehalle',
-            name: 'MiniSpielehalle',
-            component: () => import('../projects/MiniSpielehalle.vue')
-        },
-        {
-            path: '/ParkingControlSystem',
-            name: 'Parking Control System',
-            component: () => import('../projects/ParkingControlSystem.vue')
-        },
-        {
-            path: '/WebShop',
-            name: 'WebShop',
-            component: () => import('../projects/WebShop.vue')
-        },
-        {
-            path: '/DeviceManagement',
-            name: 'DeviceManagement',
-            component: () => import('../projects/DeviceManagement.vue')
-        },
-        {
-            path: '/HandInHand',
-            name: 'HandInHand',
-            component: () => import('../projects/HandInHand.vue')
-        },
-        {
-            path: '/BeGuided',
-            name: 'BeGuided',
-            component: () => import('../projects/BeGuided.vue')
-        },
-        {
-            path: '/imprint',
-            name: 'Imprint',
-            component: () => import('../views/FooterSites/Imprint.vue')
-        },
-        {
-            path: '/privacy',
-            name: 'Privacy',
-            component: () => import('../views/FooterSites/Privacy.vue')
-        },
-        // {
-        //   path: '/projects',
-        //   name: 'Projects',
-        //   component: () => import(/* webpackChunkName: "about" */ '../views/Projects.vue')
-        // },
-        // {
-        //   path: '/skills',
-        //   name: 'Skills',
-        //   component: () => import(/* webpackChunkName: "about" */ '../views/Skills.vue')
-        // },
-        // {
-        //   path: '/about',
-        //   name: 'About',
-        //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-        // },
-        // {
-        //   path: '/contact',
-        //   name: 'Contact',
-        //   component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
-        // },
-    ]
-});
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/MiniSpielehalle',
+        name: 'MiniSpielehalle',
+        component: () => import('../projects/MiniSpielehalle.vue')
+    },
+    {
+        path: '/ParkingControlSystem',
+        name: 'Parking Control System',
+        component: () => import('../projects/ParkingControlSystem.vue')
+    },
+    {
+        path: '/WebShop',
+        name: 'WebShop',
+        component: () => import('../projects/WebShop.vue')
+    },
+    {
+        path: '/DeviceManagement',
+        name: 'DeviceManagement',
+        component: () => import('../projects/DeviceManagement.vue')
+    },
+    {
+        path: '/HandInHand',
+        name: 'HandInHand',
+        component: () => import('../projects/HandInHand.vue')
+    },
+    {
+        path: '/BeGuided',
+        name: 'BeGuided',
+        component: () => import('../projects/BeGuided.vue')
+    },
+    {
+        path: '/imprint',
+        name: 'Imprint',
+        component: () => import('../views/FooterSites/Imprint.vue')
+    },
+    {
+        path: '/privacy',
+        name: 'Privacy',
+        component: () => import('../views/FooterSites/Privacy.vue')
+    },
+    {
+        path: '*', // Catch all route
+        name: 'NotFound',
+        component: () => import('../views/FooterSites/NotFound404.vue')
+    },
+]
+
+const router = new VueRouter({
+    mode: 'history', // Use history mode
+    routes
+})
+
+export default router
---
+++ src/views/FooterSites/NotFound404.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="NotFound" align="center">
+    <p class="display-3 mt-5">Seite nicht gefunden.</p>
+    <p class="mb-0">Der von Ihnen angeforderte Inhalt kann leider nicht gefunden werden. Bitte stellen Sie sicher, dass
+      Sie die richtige URL eingegeben haben oder kehren Sie zur Startseite zurück.</p>
+    <a href="https://ratzel921.github.io">
+      Gehen Sie zur Startseite zurück.
+    </a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NotFound404',
+}
+</script>
